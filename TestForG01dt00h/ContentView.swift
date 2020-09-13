ef82015cc0d7c23d4154b9b91df1457e59f9c1db
IrelDev <ireldeveloper@gmail.com>
2020-09-13 16:12:03 +0500

    Initial Commit

---
+++ TestForG01dt00h/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestForG01dt00h
+//
+//  Created by Kirill Pustovalov on 13.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
